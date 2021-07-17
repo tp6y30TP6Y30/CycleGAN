e6d61dca361281aecd1e73b3bca2f076d42b195d
tp6y30TP6Y30 <tp6y30@yahoo.com.tw>
2021-07-18 00:05:08 +0800

    add mp4 generator in GIF_generator script

--- predictions/GIF_generator.py
+++ predictions/GIF_generator.py
@@ -7,7 +7,7 @@ import os.path
 from tqdm import tqdm
 import time
 
-def generateGIFs(query, target_img_name, save_folder, interval = 20):
+def generateGIFs(query, target_img_name, save_folder, interval = 20, mode = 'gif'):
 	print('Saving GIF...')
 	print('query: ', query)
 	print('img_name: ', target_img_name)
@@ -17,14 +17,19 @@ def generateGIFs(query, target_img_name, save_folder, interval = 20):
 	print('GIF_length: ', GIF_length // interval)
 
 	images = []
-	for (index, folder_name) in enumerate(folder_names):
+	for (index, folder_name) in enumerate(folder_names, start = 1):
 		if index % interval != 0: continue
 		img_path = os.path.join(query, folder_name, target_img_name)
 		images.append(imageio.imread(img_path))
 	save_path = os.path.join(save_folder, query)
 	os.makedirs(save_path, exist_ok = True)
-	target_gif_name = target_img_name.replace('.jpg', '.gif')
-	imageio.mimsave(os.path.join(save_path, target_gif_name), images, duration = 0.2, loop = 1)
+	if mode == 'gif':
+		target_gif_name = target_img_name.replace('.jpg', '.gif')
+		imageio.mimsave(os.path.join(save_path, target_gif_name), images, duration = 0.2, loop = 1)
+	elif mode == 'mp4':
+		target_gif_name = target_img_name.replace('.jpg', '.mp4')
+		imageio.mimsave(os.path.join(save_path, target_gif_name), images, fps = 1)
+
 	print('===== {} saved ====='.format(target_gif_name))
 	print()
 
@@ -34,5 +39,5 @@ def refresh_allGIFs(query, save_folder):
 		generateGIFs(query, target_img_name, save_folder)
 
 if __name__ == '__main__':
-	# generateGIFs(query = 'horse2zebra', target_img_name = 'fake_A22.jpg', save_folder = 'GIFs/')
-	refresh_allGIFs(query = 'horse2zebra', save_folder = 'GIFs/')
+	# generateGIFs(query = 'horse2zebra', target_img_name = 'fake_A91.jpg', save_folder = 'GIFs/', mode = 'mp4')
+	# refresh_allGIFs(query = 'horse2zebra', save_folder = 'GIFs/')
